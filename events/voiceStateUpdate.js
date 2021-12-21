ffd0f4d7a1d41bd5001434a89e4764d49e184cad
Justin Lau <justin@justinlau.name>
2021-12-21 09:04:31 +0000

    Refactored config.json

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,24 +1,24 @@
-const { channelToWatchId, targetOnlineMembers } = require('../config.json')
+const { channelId, targetOnlineMembersCount, whiteListIds } = require('../config.json')
 const client = require('../index')
 
 module.exports = {
   name: 'voiceStateUpdate',
   execute(oldState, newState) {
     const { username } = newState.member.user
-    if (oldState.channelId === null && newState.channelId === channelToWatchId) {
+    if (oldState.channelId === null && newState.channelId === channelId) {
       console.log(`${username} joined specific channel`, newState.channelId)
 
-    } else if (oldState.channelId === null && newState.channelId !== channelToWatchId) {
+    } else if (oldState.channelId === null && newState.channelId !== channelId) {
       console.log(`${username} joined nonspecific channel`, newState.channelId)
 
     } else if (newState.channelId === null) {
       console.log(`${username} left channel`, oldState.channelId)
     }
 
-    const memberList = (newState.guild.channels.cache.get(channelToWatchId).members).map((member => member))
-    const { name: channelName } = newState.guild.channels.cache.get(channelToWatchId)
+    const memberList = (newState.guild.channels.cache.get(channelId).members).map((member => member))
+    const { name: channelName } = newState.guild.channels.cache.get(channelId)
 
-    if (memberList.length >= targetOnlineMembers) {
+    if (memberList.length >= targetOnlineMembersCount) {
 
       let membersOnline = []
       memberList.map((member) => {
@@ -26,12 +26,9 @@ module.exports = {
         membersOnline.push({ id, username })
       })
       const usernamesOnline = membersOnline.map(m => m.username)
-      const whiteList = [
-        '111985731932561408',
-        '786088508049195018'
-      ]
-      whiteList.map((id) => {
-        client.users.cache.get(id).send(`More than ${targetOnlineMembers} members are online in ${channelName}! ${usernamesOnline.join(', ')}`)
+
+      whiteListIds.map((id) => {
+        client.users.cache.get(id).send(`More than ${targetOnlineMembersCount} members are online in ${channelName}! ${usernamesOnline.join(', ')}`)
       })
     }
   }
