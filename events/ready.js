1de516e5882249c9db248d818a7cfdb551840003
Justin Lau <justin@justinlau.name>
2021-12-23 15:31:36 +0000

    Implemented a way to pre-fetch client user data on ready event

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,13 @@
+const { whiteListIds } = require('../config.json')
+
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
+    whiteListIds.forEach((memberId) => {
+      client.users.fetch(memberId)
+      console.log(`Client data fetched for memberId: ${memberId}`)
+    })
     console.log(`Ready! Logged in as ${client.user.tag}`)
   },
 }
